a7d838dd5b1d8a3f8eafed6d21ccc62e8de10fef
Shigma <shigma10826@gmail.com>
2022-07-06 01:34:54 +0800

    check file diff

--- build/index.ts
+++ build/index.ts
@@ -3,12 +3,32 @@ import { mkdir, writeFile } from 'fs/promises'
 import { resolve } from 'path'
 import axios from 'axios'
 
-const objects: SearchObject[] = []
+const objects: Record<string, SearchObject> = {}
 
 async function search(offset: number) {
   const { data: result } = await axios.get<SearchResult>(`https://registry.npmjs.com/-/v1/search?text=koishi+plugin&size=250&offset=${offset}`)
-  objects.push(...result.objects)
+  for (const object of result.objects) {
+    objects[object.package.name] = object
+  }
   return result.total
+}
+
+export function deepEqual(a: any, b: any) {
+  if (a === b) return true
+  if (typeof a !== typeof b) return false
+  if (typeof a !== 'object') return false
+  if (!a || !b) return false
+
+  // check array
+  if (Array.isArray(a)) {
+    if (!Array.isArray(b) || a.length !== b.length) return false
+    return a.every((item, index) => deepEqual(item, b[index]))
+  } else if (Array.isArray(b)) {
+    return false
+  }
+
+  // check object
+  return Object.keys({ ...a, ...b }).every(key => deepEqual(a[key], b[key]))
 }
 
 async function start() {
@@ -16,10 +36,27 @@ async function start() {
   for (let offset = 250; offset < total; offset += 250) {
     await search(offset)
   }
+
   const dirname = resolve(__dirname, '../dist')
-  const result: SearchResult = { total, objects, time: '' }
   await mkdir(dirname, { recursive: true })
-  writeFile(resolve(dirname, 'index.json'), JSON.stringify(result))
+  const legacy: SearchResult = require(dirname)
+
+  function hasDiff() {
+    if (total !== legacy.total) return true
+    const dict: Record<string, SearchObject> = {}
+    for (const object of legacy.objects) {
+      dict[object.package.name] = object
+    }
+    for (const name in { ...objects, ...dict }) {
+      if (!deepEqual(objects[name]?.package, dict[name]?.package)) return true
+    }
+  }
+
+  if (hasDiff()) {
+    const time = new Date().toUTCString()
+    const result: SearchResult = { total, objects: Object.values(objects), time }
+    await writeFile(resolve(dirname, 'index.json'), JSON.stringify(result))
+  }
 }
 
 if (require.main === module) {
