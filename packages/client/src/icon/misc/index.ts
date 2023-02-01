aaa6c8d168de564e56ec3e54ca2da8a7b44f0104
Shigma <shigma10826@gmail.com>
2023-02-02 03:09:25 +0800

    feat: add icons

---
+++ packages/client/src/icon/misc/index.ts
@@ -0,0 +1,25 @@
+import Balance from './balance.vue'
+import Download from './download.vue'
+import FileArchive from './file-archive.vue'
+import Insecure from './insecure.vue'
+import Newborn from './newborn.vue'
+import Preview from './preview.vue'
+import StarEmpty from './star-empty.vue'
+import StarFull from './star-full.vue'
+import StarHalf from './star-half.vue'
+import Tag from './tag.vue'
+import Verified from './verified.vue'
+
+export default {
+  'balance': Balance,
+  'download': Download,
+  'file-archive': FileArchive,
+  'insecure': Insecure,
+  'newborn': Newborn,
+  'preview': Preview,
+  'star-empty': StarEmpty,
+  'star-full': StarFull,
+  'star-half': StarHalf,
+  'tag': Tag,
+  'verified': Verified,
+}
