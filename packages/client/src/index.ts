d5c84607aa18dbfd1f7128c695b60b001a02af30
Shigma <shigma10826@gmail.com>
2023-02-02 02:31:56 +0800

    feat: add composition api

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,7 @@
 import { AnalyzedPackage, User } from '@koishijs/registry'
+import { computed, reactive } from 'vue'
 import { Dict } from 'cosmokit'
-import {} from 'vue'
+import md5 from 'spark-md5'
 
 export function getUsers(data: AnalyzedPackage) {
   const result: Record<string, User> = {}
@@ -61,4 +62,86 @@ export const categories = {
   gametool: '游戏工具',
 }
 
-export function useMarket() {}
+export function useMarket(market: () => AnalyzedPackage[]) {
+  const words = reactive([''])
+
+  const all = computed(() => {
+    return Object.values(market()).filter((data) => {
+      return !data.manifest.hidden || words.includes('show:hidden')
+    })
+  })
+
+  const packages = computed(() => {
+    return all.value.filter((data) => {
+      const users = getUsers(data)
+      return words.every(word => {
+        let negate = false
+        if (word.startsWith('-')) {
+          negate = true
+          word = word.slice(1)
+        }
+        return validate(data, word, users) !== negate
+      })
+    })
+  })
+
+  return { words, packages, all }
+}
+
+export function getAvatar(email: string, endpoint = 'https://s.gravatar.com') {
+  return endpoint
+    + '/avatar/'
+    + (email ? md5.hash(email.toLowerCase()) : '')
+    + '.png?d=mp'
+}
+
+function formatValue(value: number) {
+  return value >= 100 ? +value.toFixed() : +value.toFixed(1)
+}
+
+export function formatSize(value: number) {
+  if (value >= (1 << 20) * 1000) {
+    return formatValue(value / (1 << 30)) + ' GB'
+  } else if (value >= (1 << 10) * 1000) {
+    return formatValue(value / (1 << 20)) + ' MB'
+  } else {
+    return formatValue(value / (1 << 10)) + ' KB'
+  }
+}
+
+export function resolveCategory(name?: string) {
+  if (categories[name]) return name
+  return 'other'
+}
+
+export function validate(data: AnalyzedPackage, word: string, users: User[]) {
+  const { locales, service } = data.manifest
+  if (word.startsWith('impl:')) {
+    return service.implements.includes(word.slice(5))
+  } else if (word.startsWith('locale:')) {
+    return locales.includes(word.slice(7))
+  } else if (word.startsWith('using:')) {
+    const name = word.slice(6)
+    return service.required.includes(name) || service.optional.includes(name)
+  } else if (word.startsWith('email:')) {
+    return users.some(({ email }) => email === word.slice(6))
+  } else if (word.startsWith('updated:<')) {
+    return data.updatedAt < word.slice(9)
+  } else if (word.startsWith('updated:>')) {
+    return data.updatedAt >= word.slice(9)
+  } else if (word.startsWith('created:<')) {
+    return data.createdAt < word.slice(9)
+  } else if (word.startsWith('created:>')) {
+    return data.createdAt >= word.slice(9)
+  } else if (word.startsWith('is:')) {
+    if (word === 'is:verified') return data.verified
+    if (word === 'is:insecure') return data.insecure
+    if (word === 'is:preview') return data.manifest.preview
+    return false
+  } else if (word.startsWith('show:')) {
+    return true
+  }
+
+  if (data.shortname.includes(word)) return true
+  return data.keywords.some(keyword => keyword.includes(word))
+}
