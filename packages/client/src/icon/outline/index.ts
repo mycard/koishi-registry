d48546bd248bc6970b5aba19dcec86d98e373deb
Shigma <shigma10826@gmail.com>
2023-02-02 03:22:31 +0800

    chore: update icons

--- packages/client/src/icon/outline/index.ts
+++ packages/client/src/icon/outline/index.ts
@@ -3,16 +3,15 @@ import Ai from './ai.vue'
 import Console from './console.vue'
 import Core from './core.vue'
 import Extension from './extension.vue'
-import Fun from './fun.vue'
 import Game from './game.vue'
 import Gametool from './gametool.vue'
 import Image from './image.vue'
 import Manage from './manage.vue'
+import Media from './media.vue'
+import Meme from './meme.vue'
 import Other from './other.vue'
 import Preset from './preset.vue'
-import Search from './search.vue'
 import Storage from './storage.vue'
-import Subscribe from './subscribe.vue'
 import Tool from './tool.vue'
 
 export default {
@@ -21,15 +20,14 @@ export default {
   'outline:console': Console,
   'outline:core': Core,
   'outline:extension': Extension,
-  'outline:fun': Fun,
   'outline:game': Game,
   'outline:gametool': Gametool,
   'outline:image': Image,
   'outline:manage': Manage,
+  'outline:media': Media,
+  'outline:meme': Meme,
   'outline:other': Other,
   'outline:preset': Preset,
-  'outline:search': Search,
   'outline:storage': Storage,
-  'outline:subscribe': Subscribe,
   'outline:tool': Tool,
 }
--- packages/client/src/icon/solid/index.ts
+++ packages/client/src/icon/solid/index.ts
@@ -3,16 +3,15 @@ import Ai from './ai.vue'
 import Console from './console.vue'
 import Core from './core.vue'
 import Extension from './extension.vue'
-import Fun from './fun.vue'
 import Game from './game.vue'
 import Gametool from './gametool.vue'
 import Image from './image.vue'
 import Manage from './manage.vue'
+import Media from './media.vue'
+import Meme from './meme.vue'
 import Other from './other.vue'
 import Preset from './preset.vue'
-import Search from './search.vue'
 import Storage from './storage.vue'
-import Subscribe from './subscribe.vue'
 import Tool from './tool.vue'
 
 export default {
@@ -21,15 +20,14 @@ export default {
   'solid:console': Console,
   'solid:core': Core,
   'solid:extension': Extension,
-  'solid:fun': Fun,
   'solid:game': Game,
   'solid:gametool': Gametool,
   'solid:image': Image,
   'solid:manage': Manage,
+  'solid:media': Media,
+  'solid:meme': Meme,
   'solid:other': Other,
   'solid:preset': Preset,
-  'solid:search': Search,
   'solid:storage': Storage,
-  'solid:subscribe': Subscribe,
   'solid:tool': Tool,
 }
