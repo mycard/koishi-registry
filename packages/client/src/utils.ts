22c0da3efd91bdf28c6cfb13d98f23f7201371e9
Shigma <shigma10826@gmail.com>
2023-03-12 16:00:39 +0800

    fix: skip users withuot email

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -5,6 +5,7 @@ import { Dict } from 'cosmokit'
 export function getUsers(data: AnalyzedPackage) {
   const result: Record<string, User> = {}
   for (const user of data.contributors) {
+    if (!user.email) continue
     result[user.email] ||= user
   }
   if (!data.maintainers.some(user => result[user.email])) {
